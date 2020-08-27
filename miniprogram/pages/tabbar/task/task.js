f369cf944c7d5bcce89449c8a13c8fe94c595a49
patrikhuang <patrikhuang@tencent.com>
2020-08-27 12:12:25 +0800

    松柏印记 0.04
    添加swiper，联动需要进一步调整

--- miniprogram/pages/tabbar/task/task.js
+++ miniprogram/pages/tabbar/task/task.js
@@ -5,7 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
+    active: 1,
 
+    background: [
+      { "img": "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"},
+      { "img": "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"}
+    ],
+    indicatorDots: false,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500
+  },
+  
+  onChange(event) {
+    console.log(event.detail.index);
+    if (event.detail.index == '0') {
+      wx.navigateTo({
+        url: 'doingTask/doingTask',
+      })
+    } else {
+      wx.navigateTo({
+        url: 'overTask/overTask',
+      })
+    }
+    // wx.showToast({
+    //   title: `切换到标签 ${event.detail}`,
+    //   icon: 'none',
+    // });
   },
 
   /**
