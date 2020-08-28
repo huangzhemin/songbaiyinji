f03deb650577be0ca16a6eb81b0a8be1e251826a
patrikhuang <patrikhuang@tencent.com>
2020-08-28 17:46:46 +0800

    松柏印记 0.1.3
    cell跳转添加

--- miniprogram/components/taskComponents/doingTaskCell/doingTaskCell.js
+++ miniprogram/components/taskComponents/doingTaskCell/doingTaskCell.js
@@ -11,7 +11,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-    title: '测试标题,测试标题',
+    title: '测试标题,测试标题,测试标题,测试标题',
     desc: '测试描述，这是一条很长的描述很长的描述很长的描述很长的描述',
     tag: '标签',
     status: 0
@@ -28,6 +28,10 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onClick(event) {
+      wx.navigateTo({
+        url: '../../../pages/taskDetail/taskDetail',
+      })
+    }
   }
 })
