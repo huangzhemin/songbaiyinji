b54c9b2055bff04db1838cabc36ee388ecbf14bc
patrikhuang <patrikhuang@tencent.com>
2020-09-01 23:08:13 +0800

    松柏印记 0.2.1
    保持客人页 和 主人页合并，给我的tab未来预留扩展空间，pm-user-detail-task-list待完善

---
+++ miniprogram/components/personalComponents/userDetailTaskList/userDetailTaskList.js
@@ -0,0 +1,23 @@
+// components/personalComponents/userDetailTaskList/userDetailTaskList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- miniprogram/pages/tabbar/personal/personal.js
+++ miniprogram/pages/tabbar/personal/personal.js
@@ -5,43 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    taskList: [
-      {
-        taskTitle:'这是任务标题任务标题任务标题',
-        taskDesc: '这是任务描述这是任务描述这是任务描述这是任务描述',
-        status: '0',
-        thumbImg: '/images/create-collection.png',
-      },
-      {
-        taskTitle:'这是任务标题任务标题任务标题',
-        taskDesc: '这是任务描述这是任务描述这是任务描述这是任务描述',
-        status: '1',
-        thumbImg: '/images/create-collection.png',
-      },
-      {
-        taskTitle:'这是任务标题任务标题任务标题',
-        taskDesc: '这是任务描述这是任务描述这是任务描述这是任务描述',
-        status: '3',
-        thumbImg: '/images/create-collection.png',
-      },
-    ],
-    userInfo: {
-      sdkUserInfo: {
-        avatarUrl: 'https://b.yzcdn.cn/vant/icon-demo-1126.png',
-        nickName: '小黄',
-      },
-      customUserInfo: {
-        userRanking: '1',
-        userPoints: '100',
-      },
-    }
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
@@ -55,6 +26,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+
   },
 
   /**
@@ -84,8 +56,6 @@ Page({
   onReachBottom: function () {
 
   },
-
-  
 
   /**
    * 用户点击右上角分享
