275b47031e94751bf1bc47e9cf79adf9d16dcdda
patrikhuang <patrikhuang@tencent.com>
2020-09-02 17:58:33 +0800

    松柏印记 0.2.5
    底层页真实数据接入，下一步完善所有的任务底层页

--- miniprogram/pages/taskDetail/taskDetail.js
+++ miniprogram/pages/taskDetail/taskDetail.js
@@ -1,4 +1,8 @@
 // miniprogram/pages/taskDetail/taskDetail.js
+const db = wx.cloud.database()
+const taskInfo = db.collection('taskInfo')
+var util = require('../../util.js')
+
 Page({
 
   /**
@@ -7,89 +11,45 @@ Page({
   data: {
     taskTitle: "",
     taskPlan: {
-      taskDesc: "就爱上了咖啡骄傲戴老师开发老大时空裂缝家傲的世间法开始发酵",
+      taskDesc: "",
       uploadMediaList: [],
       showUpload: true,
     },
     taskComplete: {
-      taskDesc: "1克里斯丁监控了对方水晶开发当时房价第三个会撒娇可感受到科技公司看反馈",
+      taskDesc: "",
       uploadMediaList: [],
       showUpload: true,
     },
+    taskMediaList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+    wx.showLoading({
+      title: 'loading...',
+    })
+    taskInfo.doc('f241f5fe5f4f67ab0011a3e826d8a99d').get().then(res => {
+      console.log(res);
+      this.data.taskMediaList = res.data['taskMediaList'];
+      let uploadMediaListDic = util.getUploadMediaList(this.data.taskMediaList);
+      this.setData({
+        taskTitle: res.data['taskTitle'],
+        ['taskPlan.taskDesc']: res.data['taskPlanDesc'],
+        ['taskPlan.uploadMediaList']: uploadMediaListDic['taskPlan'],
+        ['taskComplete.taskDesc']: res.data['taskCompleteDesc'],
+        ['taskComplete.uploadMediaList']: uploadMediaListDic['taskComplete'],
+      });
+    }).then(res1 => {
+      wx.hideLoading();
+    })
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  },
-
-  afterRead(event) {
-    console.log(event.detail);
-    // this.setData({ fileList });
-    // const { file } = event.detail;
-    // // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
-    // wx.uploadFile({
-    //   url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
-    //   filePath: file.path,
-    //   name: 'file',
-    //   formData: { user: 'test' },
-    //   success(res) {
-    //     // 上传完成需要更新 fileList
-    //     const { fileList = [] } = this.data;
-    //     fileList.push({ ...file, url: res.data });
-    //     this.setData({ fileList });
-    //   },
-    // });
   },
 
   chooseImage: function(event) {
@@ -115,16 +75,5 @@ Page({
       console.log(event);
       console.log(err)
     })
-    // var that = this;
-    //     wx.chooseImage({
-    //         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-    //         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-    //         success: function (res) {
-    //             // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-    //             that.setData({
-    //                 files: that.data.files.concat(res.tempFilePaths)
-    //             });
-    //         }
-    //     })
   }
 })
--- miniprogram/util.js
+++ miniprogram/util.js
@@ -22,7 +22,34 @@ var convertDatabaseTaskToInnerTask = function(databaseTask) {
   return innerTaskData;
 }
 
+//该方法，传入taskMediaList，标识数据库媒体列表
+//将处理并且排序后的 plan和complete两个list打包返回
+var getUploadMediaList = function(taskMediaList) {
+  let taskPlanUploadMediaList = [];
+  let taskCompleteUploadMediaList = [];
+  for (let index = 0; index < taskMediaList.length; index++) {
+    const element = taskMediaList[index];
+    //warning 此处需要带入真实的用户openid 和 taskid，标记一下
+    //匹配字符串，定位
+    if (element.match('openid_taskid_plan')) {
+      let i = element.charAt(element.search('plan_')+5);
+      //因为只有9张，所以可以采用这种tricky方法
+      taskPlanUploadMediaList[i] = element;
+    } else if (element.match('openid_taskid_complete')) {
+      let i = element.charAt(element.search('complete_')+9);
+      //因为只有9张，所以可以采用这种tricky方法
+      taskCompleteUploadMediaList[i] = element;
+    }
+  }
+
+  return {
+    'taskPlan': taskPlanUploadMediaList,
+    'taskComplete': taskCompleteUploadMediaList,
+  }; 
+}
+
 module.exports={
   convertInnerTaskToDatabaseTask: convertInnerTaskToDatabaseTask,
   convertDatabaseTaskToInnerTask: convertDatabaseTaskToInnerTask,
+  getUploadMediaList: getUploadMediaList,
 }
