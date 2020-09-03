20865b9bf85b31f1122dd88af782d1ce36838736
patrikhuang <patrikhuang@tencent.com>
2020-09-03 15:32:20 +0800

    松柏印记 0.3.6
    同步猜一猜 和 投票 底层页参数传递，下一步底层页样式关联用户openid，根据用户类型做区分展示「自身」「其他用户」「有投票能力的志愿者」

--- miniprogram/pages/voteDetail/voteDetail.js
+++ miniprogram/pages/voteDetail/voteDetail.js
@@ -44,22 +44,36 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //获取传入参数
+    const eventChannel = this.getOpenerEventChannel()
+    var currentOpenId = '';
+    var currentTaskId = '';
+    // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+    eventChannel.on('acceptDataFromOpenerPage', function(data) {
+      currentOpenId = data.openId;
+      currentTaskId = data.taskId;
+    })
+
     wx.showLoading({
       title: 'loading...',
     })
-    taskInfo.doc('74b3e15b5f5061710014e0920a7545f2').get().then(res => {
-      this.data.taskMediaList = res.data['taskMediaList'];
+    //通过传入的openId和taskId，拉取用户数据
+    taskInfo.where({
+      openId: currentOpenId,
+      taskId: currentTaskId,
+    }).get().then(res => {
+      this.data.taskMediaList = res.data[0]['taskMediaList'];
       let uploadMediaListDic = util.getUploadMediaList(this.data.taskMediaList);
       this.setData({
-        taskTitle: res.data['taskTitle'],
-        ['taskPlan.taskDesc']: res.data['taskPlanDesc'],
+        taskTitle: res.data[0]['taskTitle'],
+        ['taskPlan.taskDesc']: res.data[0]['taskPlanDesc'],
         ['taskPlan.uploadMediaList']: uploadMediaListDic['taskPlan'],
-        ['taskComplete.taskDesc']: res.data['taskCompleteDesc'],
+        ['taskComplete.taskDesc']: res.data[0]['taskCompleteDesc'],
         ['taskComplete.uploadMediaList']: uploadMediaListDic['taskComplete'],
       });
     }).then(res1 => {
       wx.hideLoading();
-    })
+    });
   },
 
   /**
